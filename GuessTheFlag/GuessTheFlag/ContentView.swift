c024f8c47292dd54b3e7cdc333d1ddf0979e2e8b
Jane Madsen <janemadsen815@gmail.com>
2023-02-24 09:42:56 -0700

    Clear practice work to begin main project details

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Jane Madsen on 2/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showingAlert = false
+    
+    var body: some View {
+        VStack {
+            Text("Hey")
+        }
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Jane Madsen on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
